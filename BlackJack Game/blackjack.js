e4073a74615252a02dbb72dc1178e1786f8daa34
BryceLikesDogs <36935978+BryceLikesDogs@users.noreply.github.com>
2018-03-13 14:30:42 +1300

    New
    Setting up

---
+++ BlackJack Game/blackjack.js
@@ -0,0 +1,37 @@
+function controller() {
+  //initilise game
+  // handles button presses
+  //
+  // while count < 21{
+  //   when draw presed
+  //       draw
+  //       check
+  //draw(computer)
+  //draw(computer)
+  //compares scores
+  //says who wins
+
+
+}
+
+
+function createDeck () {
+  //creates an array of 52 cards
+  //return deck
+}
+
+function draw (deck, hand) {
+// choose random number from 0 - array.length-1
+// remove selected card from deck and put into hand
+
+}
+
+function check(hand) {
+//returns count of hand
+
+}
+
+function initilise(){
+  // creates deck
+  // draws two cards
+}
