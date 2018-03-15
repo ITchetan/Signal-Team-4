abf179fc32b9dbd23fa371e7ef04826a20999d4d
Chetan Baadkar <37316635+ITchetan@users.noreply.github.com>
2018-03-16 09:34:37 +1300

    work in process
    work in progress

--- BlackJack Game/Scripts/gameObject.js
+++ BlackJack Game/Scripts/gameObject.js
@@ -9,16 +9,24 @@ class GameObject {
     user.cardDraw(newDeck, user)
 
   }
- console.log(user.cardDraw(newDeck, user))
- var userSum = user.handCount()
+ //console.log(user.cardDraw(newDeck, user))
+ //var userSum = user.handCount()
 
  //console.log(userSum)
   // Methods
+
+  cardValueCount(this.User){
+    var userCardVaue = user.handCount;
+    console.log(userCardVaue);
+  }
+
 
 
   compareCardValue () {// work in progress
     // compares the hand count of all players and return the winer
     // Work in progress Chetan
+
+    console.log("print");
   }
   displayWinner() {// work in progress
     // displays the winning message work
