01570d7ece8ee2618761e9828bed53b88b615bcb
BryceLikesDogs <36935978+BryceLikesDogs@users.noreply.github.com>
2018-03-15 13:10:06 +1300

    Moved methods
    JUst moved some methods around between classes

--- BlackJack Game/Scripts/gameObject.js
+++ BlackJack Game/Scripts/gameObject.js
@@ -10,12 +10,11 @@ class GameObject {
 
   // Methods
 
-  cardDraw (deck, target) {
-    // draws a card from the deck and adds to hand
-    var randNum = Math.floor(Math.random() * 51) + 1
-    randNum = Number(randNum)
-    console.log(deck.decklist[randNum])
-    var dog = deck.decklist[randNum]
-    target.hand.push(dog)
+
+  compareHandCount() {
+    //compares the hand count of all players and return the winer
   }
+
+  displayWinner() {
+    //displays the winning message
 }
--- BlackJack Game/Scripts/player.js
+++ BlackJack Game/Scripts/player.js
@@ -18,12 +18,17 @@ class Player {
     console.log(this.hand)
   }
 
-  compareHandCount() {
-    //compares the hand count of all players and return the winer
+  cardDraw (deck) {
+    // draws a card from the deck and adds to hand
+    var randNum = Math.floor(Math.random() * 51) + 1
+    randNum = Number(randNum)
+    console.log(deck.decklist[randNum])
+    // dog is just to temporarily store the values of the card. Could be called card but would
+    // get confusing having a million variables called card
+    var dog = deck.decklist[randNum]
+    this.hand.push(dog)
   }
 
-  displayWinner() {
-    //displays the winning message
 
   }
 
