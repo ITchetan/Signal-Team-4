b219ff5047d0af5fe7fb893d3b6e2870dad9783c
KirianaBrown <kjb0369@arastudent.ac.nz>
2018-03-19 13:35:06 +1300

    Revert "Merge branch 'master' into Kiriana-"
    This reverts commit 8336cdb9f0ac44a9395e3ad23beb6b62d50b1742, reversing
    changes made to 9a220dc7bc4a5705653eeedd9fd721011c945fdb.

--- BlackJack Game/Scripts/card.js
+++ BlackJack Game/Scripts/card.js
@@ -1,11 +1,10 @@
 class Card {
-  constructor (value, suit, cardValue) {
-    var suits = ['spades', 'clubs', 'diamonds', 'hearts']
-    var values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
-    var cardValues = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+  constructor (value, suit) {
+    var suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+    var values = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
     this.value = values[value]
     this.suit = suits[suit]
-    this.image = 'Images/Cards/' + this.value + '_of_' + this.suit + '.svg'
-    this.cardValue = cardValues[cardValue]
+    //image;
   }
 }
--- BlackJack Game/Scripts/player.js
+++ BlackJack Game/Scripts/player.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Player {
   constructor (name) {
     this.name = name
@@ -28,52 +27,4 @@ class Player {
 
   }
 
-=======
- class Player {
-  constructor () {
-    this.name = name
-    this.hand = []
-    this.handCount = 0
-    this.wins = 0
-  }
-
-  cardDraw (deck) {
-    // draws a card from the deck and adds to hand and removes that same card from the decklist
-    var randNum = Math.floor(Math.random() * (newDeck.decklist.length - 1)) + 1
-    randNum = Number(randNum)
-    var card = deck.decklist[randNum]
-    this.hand.push(card)
-    deck.decklist.splice(randNum, 1)
-    if (this.handCount > 10 && card.cardValue ==11) {
-      card.cardValue = 1
-    }
-
-    this.handCount += card.cardValue
-
-    return card
-    if (this.handCount > 21) {
-      compareScore();
-    }
-  }
-
-  // bustPlayer(){
-  //   if (this.handCount >21) {
-  //     return True;
-  //   }
-  // }
-
-  displayCard (card) {
-    // returns a string which is the location of the card image
-    var cardImage = card.image
-    // console.log(card.image)
-    return String(cardImage)
-  }
-
-  displayReset () {
-    // returns a string which is the location of the card image
-    var cardImage = ""
-    // console.log(card.image)
-    return String(cardImage)
-  }
->>>>>>> master
 }
